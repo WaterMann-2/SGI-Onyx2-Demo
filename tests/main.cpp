10c1d61521cdeb9aab9549c28ec7c8f2ac154bfb
Wattermann-2 <rrschad2004@gmail.com>
2026-06-03 01:03:08 -0500

    Fix to let glm be addressable outside of graphics-engine

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,5 @@
 #include <Graphics.h>
 
-
 int main(int argc,char* argv[]) {
 	SpEngine::Graphics graphics;
 
@@ -12,9 +11,6 @@ int main(int argc,char* argv[]) {
 	windowContext.fullscreen = false;
 	windowContext.clearColor = glm::vec4(10.0f, 10.0f, 50.0f, 255.0f);
 
-	glm::vec<4, float> awesomeColor = glm::vec<4, float>(1.0f);
-	glm::vec<4, float> newAwesome = glm::vec<4, float>(1.0f);
-	awesomeColor = newAwesome;
 
 	graphics.Init(windowContext);
 	graphics.MainLoop();
